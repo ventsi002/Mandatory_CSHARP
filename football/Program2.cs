69ad31bf14457f31027a2a18574a9285e9ab78ba
Daniel <dannysht27@gmail.com>
2023-04-06 15:57:57 +0200

    added parsing, try catches and and fixed some visual stuff

--- football/Program2.cs
+++ football/Program2.cs
@@ -5,6 +5,24 @@ using System.Linq;
 
 class Program2
 {
+    public static int RoundInput()
+    {
+            try
+            {
+                Console.Write("Enter the number of rounds: ");
+                int rounds = int.Parse(Console.ReadLine()!);
+                if(rounds > 32)
+                {
+                    throw new Exception();
+                }
+                return rounds;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("No such round, valid rounds are 1-32");
+                return RoundInput();
+            }
+    }
     static void Main(string[] args)
     {
         // Read the teams file
@@ -25,90 +43,96 @@ class Program2
         }).Where(team => team != null).ToList();
 
         // Read the rounds files and calculate team stats
-        Console.Write("Enter the number of rounds: ");
-        int numberRounds = int.Parse(Console.ReadLine());
+        int numberRounds = Program2.RoundInput();
         var rounds = Enumerable.Range(1, numberRounds);
         foreach (var round in rounds)
         {
-            var roundLines = File.ReadAllLines($"round-{round}.csv").Skip(1);
-            foreach (var line in roundLines)
+            try
             {
-                var fields = line.Split(';');
-                var homeTeam = teams.FirstOrDefault(team => team?.Name == fields[0] || team?.Abbreviation == fields[0]);
-                var awayTeam = teams.FirstOrDefault(team => team?.Name == fields[1] || team?.Abbreviation == fields[1]);
-                if (homeTeam != null && awayTeam != null)
+                var roundLines = File.ReadAllLines($"round-{round}.csv").Skip(1);
+                foreach (var line in roundLines)
                 {
-                    var homeGoals = int.Parse(fields[2]);
-                    var awayGoals = int.Parse(fields[3]);
-                    homeTeam.GamesPlayed++;
-                    awayTeam.GamesPlayed++;
-                    homeTeam.GoalsFor += homeGoals;
-                    awayTeam.GoalsFor += awayGoals;
-                    homeTeam.GoalsAgainst += awayGoals;
-                    awayTeam.GoalsAgainst += homeGoals;
-                    homeTeam.GoalDifference += homeGoals - awayGoals;
-                    awayTeam.GoalDifference += awayGoals - homeGoals;
-                    if (homeGoals > awayGoals)
+                    var fields = line.Split(';');
+                    var homeTeam = teams.FirstOrDefault(team => team?.Name == fields[0] || team?.Abbreviation == fields[0]);
+                    var awayTeam = teams.FirstOrDefault(team => team?.Name == fields[1] || team?.Abbreviation == fields[1]);
+                    if (homeTeam != null && awayTeam != null)
                     {
-                        homeTeam.GamesWon++;
-                        awayTeam.GamesLost++;
-                        homeTeam.Points += 3;
-                        homeTeam.LastFiveResults.Enqueue("W");
-                        if (homeTeam.LastFiveResults.Count > 5)
+                        var homeGoals = int.Parse(fields[2]);
+                        var awayGoals = int.Parse(fields[3]);
+                        homeTeam.GamesPlayed++;
+                        awayTeam.GamesPlayed++;
+                        homeTeam.GoalsFor += homeGoals;
+                        awayTeam.GoalsFor += awayGoals;
+                        homeTeam.GoalsAgainst += awayGoals;
+                        awayTeam.GoalsAgainst += homeGoals;
+                        homeTeam.GoalDifference += homeGoals - awayGoals;
+                        awayTeam.GoalDifference += awayGoals - homeGoals;
+                        if (homeGoals > awayGoals)
                         {
-                            homeTeam.LastFiveResults.Dequeue();
+                            homeTeam.GamesWon++;
+                            awayTeam.GamesLost++;
+                            homeTeam.Points += 3;
+                            homeTeam.LastFiveResults.Enqueue("W");
+                            if (homeTeam.LastFiveResults.Count > 5)
+                            {
+                                homeTeam.LastFiveResults.Dequeue();
+                            }
+                            homeTeam.CurrentStreak = string.Join("", homeTeam.LastFiveResults.Reverse());
+                            awayTeam.LastFiveResults.Enqueue("L");
+                            if (awayTeam.LastFiveResults.Count > 5)
+                            {
+                                awayTeam.LastFiveResults.Dequeue();
+                            }
+                            awayTeam.CurrentStreak = string.Join("", awayTeam.LastFiveResults.Reverse());
                         }
-                        homeTeam.CurrentStreak = string.Join("", homeTeam.LastFiveResults.Reverse());
-                        awayTeam.LastFiveResults.Enqueue("L");
-                        if (awayTeam.LastFiveResults.Count > 5)
+                        else if (homeGoals < awayGoals)
                         {
-                            awayTeam.LastFiveResults.Dequeue();
+                            awayTeam.GamesWon++;
+                            homeTeam.GamesLost++;
+                            awayTeam.Points += 3;
+                            awayTeam.LastFiveResults.Enqueue("W");
+                            if (awayTeam.LastFiveResults.Count > 5)
+                            {
+                                awayTeam.LastFiveResults.Dequeue();
+                            }
+                            awayTeam.CurrentStreak = string.Join("", awayTeam.LastFiveResults.Reverse());
+                            homeTeam.LastFiveResults.Enqueue("L");
+                            if (homeTeam.LastFiveResults.Count > 5)
+                            {
+                                homeTeam.LastFiveResults.Dequeue();
+                            }
+                            homeTeam.CurrentStreak = string.Join("", homeTeam.LastFiveResults.Reverse());
                         }
-                        awayTeam.CurrentStreak = string.Join("", awayTeam.LastFiveResults.Reverse());
-                    }
-                    else if (homeGoals < awayGoals)
-                    {
-                        awayTeam.GamesWon++;
-                        homeTeam.GamesLost++;
-                        awayTeam.Points += 3;
-                        awayTeam.LastFiveResults.Enqueue("W");
-                        if (awayTeam.LastFiveResults.Count > 5)
+                        else
                         {
-                            awayTeam.LastFiveResults.Dequeue();
+                            homeTeam.GamesDrawn++;
+                            awayTeam.GamesDrawn++;
+                            homeTeam.Points += 1;
+                            awayTeam.Points += 1;
+                            homeTeam.LastFiveResults.Enqueue("D");
+                            if (homeTeam.LastFiveResults.Count > 5)
+                            {
+                                homeTeam.LastFiveResults.Dequeue();
+                            }
+                            homeTeam.CurrentStreak = string.Join("", homeTeam.LastFiveResults.Reverse());
+                            awayTeam.LastFiveResults.Enqueue("D");
+                            if (awayTeam.LastFiveResults.Count > 5)
+                            {
+                                awayTeam.LastFiveResults.Dequeue();
+                            }
+                            awayTeam.CurrentStreak = string.Join("", awayTeam.LastFiveResults.Reverse());
                         }
-                        awayTeam.CurrentStreak = string.Join("", awayTeam.LastFiveResults.Reverse());
-                        homeTeam.LastFiveResults.Enqueue("L");
-                        if (homeTeam.LastFiveResults.Count > 5)
-                        {
-                            homeTeam.LastFiveResults.Dequeue();
-                        }
-                        homeTeam.CurrentStreak = string.Join("", homeTeam.LastFiveResults.Reverse());
+
                     }
                     else
                     {
-                        homeTeam.GamesDrawn++;
-                        awayTeam.GamesDrawn++;
-                        homeTeam.Points += 1;
-                        awayTeam.Points += 1;
-                        homeTeam.LastFiveResults.Enqueue("D");
-                        if (homeTeam.LastFiveResults.Count > 5)
-                        {
-                            homeTeam.LastFiveResults.Dequeue();
-                        }
-                        homeTeam.CurrentStreak = string.Join("", homeTeam.LastFiveResults.Reverse());
-                        awayTeam.LastFiveResults.Enqueue("D");
-                        if (awayTeam.LastFiveResults.Count > 5)
-                        {
-                            awayTeam.LastFiveResults.Dequeue();
-                        }
-                        awayTeam.CurrentStreak = string.Join("", awayTeam.LastFiveResults.Reverse());
+                        Console.WriteLine($"One or both of the teams in line {line} are not registered.");
                     }
-
-                }
-                else
-                {
-                    Console.WriteLine($"One or both of the teams in line {line} are not registered.");
-                }
+                }   
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("No rounds after the 32nd");
             }
         }
 
@@ -133,7 +157,7 @@ class Program2
         }
 
         // Write the standings file
-        Console.WriteLine("{0,-3}{1,-25}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-5}{9,-4}{10,-10}", "Pos", "Team", "GP", "W", "L", "D", "GF", "GA", "GD", "Pts", "Streak");
+        Console.WriteLine("{0,-4}{1,-25}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-5}{9,-4}{10,-10}", "Pos", "Team", "GP", "W", "L", "D", "GF", "GA", "GD", "Pts", "Streak");
         for (int i = 0; i < sortedTeams.Count; i++)
         {
             var team = sortedTeams[i];
@@ -151,13 +175,41 @@ class Program2
             {
                 Console.ForegroundColor = ConsoleColor.Red;
             }
-
-            // Write team row
-            Console.WriteLine("{0,-3}{1,-25}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-5}{9,-4}{10,-10}", i + 1, team?.Name, team?.GamesPlayed, team?.GamesWon, team?.GamesLost, team?.GamesDrawn, team?.GoalsFor, team?.GoalsAgainst, team?.GoalDifference, team?.Points, team?.CurrentStreak);
+            if(numberRounds < 23)
+            {   
+                // Write team row
+                Console.WriteLine("{0,-4}{1,-25}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-5}{9,-4}{10,-10}", i + 1, team?.Name, team?.GamesPlayed, team?.GamesWon, team?.GamesLost, team?.GamesDrawn, team?.GoalsFor, team?.GoalsAgainst, team?.GoalDifference, team?.Points, team?.CurrentStreak);
+            }
+            else
+            {
+                if(i < 6)
+                {
+                    Console.WriteLine("{0,-4}{1,-25}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-5}{9,-4}{10,-10}", i + 1, team?.Name, team?.GamesPlayed, team?.GamesWon, team?.GamesLost, team?.GamesDrawn, team?.GoalsFor, team?.GoalsAgainst, team?.GoalDifference, team?.Points, team?.CurrentStreak);
+                }
+                else
+                {
+                    if(i == 6)
+                    {
+                        Console.WriteLine("");
+                    }
+                    Console.WriteLine("{0,-4}{1,-25}{2,-3}{3,-3}{4,-3}{5,-3}{6,-3}{7,-4}{8,-5}{9,-4}{10,-10}", i - 5, team?.Name, team?.GamesPlayed, team?.GamesWon, team?.GamesLost, team?.GamesDrawn, team?.GoalsFor, team?.GoalsAgainst, team?.GoalDifference, team?.Points, team?.CurrentStreak);
+                }
+            }
+            
 
             // Reset console color
             Console.ResetColor();
         }
+                // Write the standings file
+            var standingsLines = new List<string>();
+            standingsLines.Add("Team,GP,W,L,D,GF,GA,GD,Pts,Streak");
+            foreach (var team in sortedTeams)
+            {
+                standingsLines.Add($"{team.Name},{team.GamesPlayed},{team.GamesWon},{team.GamesLost},{team.GamesDrawn},{team.GoalsFor},{team.GoalsAgainst},{team.GoalDifference},{team.Points},{team.CurrentStreak}");
+            }
+            File.WriteAllLines("standings.csv", standingsLines);
+
+            Console.WriteLine("Standings have been calculated and written to standings.csv.");
     }
 }
 
